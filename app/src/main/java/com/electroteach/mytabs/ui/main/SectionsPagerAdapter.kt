be5c9b5b64387d96cc52c7c6a06b5c8bd7f1ae8b
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-06-18 10:59:28 +1000

    start of changes and kotlin experiments

--- app/src/main/java/com/electroteach/mytabs/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/electroteach/mytabs/ui/main/SectionsPagerAdapter.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.electroteach.mytabs.R
+import kotlin.Int as KotlinInt
 
 private val TAB_TITLES = arrayOf(
         R.string.tab_text_1,
-        R.string.tab_text_2
+        R.string.tab_text_2,
+        "Tab 3"
 )
 
 /**
@@ -18,17 +20,17 @@ private val TAB_TITLES = arrayOf(
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment {
+    override fun getItem(position: KotlinInt): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+    override fun getPageTitle(position: KotlinInt): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position] as KotlinInt)
     }
 
-    override fun getCount(): Int {
+    override fun getCount(): KotlinInt {
         // Show 2 total pages.
         return 2
     }
