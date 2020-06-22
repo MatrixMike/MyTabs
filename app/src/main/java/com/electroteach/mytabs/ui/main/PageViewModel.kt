a5c6f9d76e410c15a9f7a267da22b29a9b796e37
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-06-22 15:04:06 +1000

    some changes and kotlin experiments 6

--- app/src/main/java/com/electroteach/mytabs/ui/main/PageViewModel.kt
+++ app/src/main/java/com/electroteach/mytabs/ui/main/PageViewModel.kt
@@ -1,16 +1,23 @@
 package com.electroteach.mytabs.ui.main
 
+import android.os.Build
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import java.time.LocalDateTime
 
 class PageViewModel : ViewModel() {
-
+    val exp1 = String.format("   ")
+    val currentPVM = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        LocalDateTime.now()
+    } else {
+        TODO("VERSION.SDK_INT < O")
+    }
     private val _index = MutableLiveData<Int>()
     val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
+        "Hello world from section: $it  " + currentPVM
     }
 
     fun setIndex(index: Int) {
