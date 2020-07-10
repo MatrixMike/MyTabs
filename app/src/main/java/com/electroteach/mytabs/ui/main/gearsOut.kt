3199f11c21b183850b809440ac41a7ad9d669192
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-07-10 15:19:49 +1000

    added sorted ratio  2

--- app/src/main/java/com/electroteach/mytabs/ui/main/gearsOut.kt
+++ app/src/main/java/com/electroteach/mytabs/ui/main/gearsOut.kt
@@ -3,7 +3,7 @@ fun gearsOut (): String {
     val gearsRatio: MutableList<Double> = mutableListOf()
     var formatted =""
     val frontGears = listOf(42.0, 32.0, 22.0)   // deliberately not sorted
-    val rearGears = listOf(26.0, 23.0, 21.0, 19.0, 17.0, 15.0, 13.0, 12.0)  // number of teeth on gear is an integer
+    val rearGears  = listOf(26.0, 23.0, 21.0, 19.0, 17.0, 15.0, 13.0, 12.0)  // number of teeth on gear is an integer
     for (f in frontGears){
         for (r in rearGears){
             formatted = formatted + String.format("*%.2f   %.2f   %.2f   \n", f/r, f, r ) ;
